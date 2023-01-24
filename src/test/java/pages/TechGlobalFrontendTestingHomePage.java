ae3b55eac2f0db780f6f04f5473763fe530ce888
ReginaIulgut <reginaiulgut@gmail.com>
2023-01-23 23:11:41 -0600

    project added

---
+++ src/test/java/pages/TechGlobalFrontendTestingHomePage.java
@@ -0,0 +1,20 @@
+package pages;
+
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.FindBy;
+import org.openqa.selenium.support.PageFactory;
+
+import java.util.List;
+
+public class TechGlobalFrontendTestingHomePage extends TechGlobalBasePage {
+    public TechGlobalFrontendTestingHomePage() {
+        super();
+    }
+
+    @FindBy(css="div[id^='card']")
+    public List<WebElement> cards;
+
+    public void clickOnCard(int cardIndex) {
+        cards.get(cardIndex).click();
+    }
+}
---
+++ src/test/java/pages/TechGlobalLoginFormPage.java
@@ -0,0 +1,58 @@
+package pages;
+
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.FindBy;
+
+public class TechGlobalLoginFormPage extends TechGlobalBasePage {
+    public TechGlobalLoginFormPage() {
+        super();
+    }
+
+    @FindBy(id = "main_heading")
+    public WebElement heading;
+
+    @FindBy(css = "label[for='username']")
+    public WebElement usernameLabel;
+
+    @FindBy(id = "username")
+    public WebElement usernameInputBox;
+
+    @FindBy(css = "label[for='password']")
+    public WebElement passwordLabel;
+
+    @FindBy(id = "password")
+    public WebElement passwordInputBox;
+
+    @FindBy(id = "login_btn")
+    public WebElement loginButton;
+
+    @FindBy(id = "logout")
+    public WebElement logoutButton;
+
+    @FindBy(id = "forgot-password")
+    public WebElement forgotPasswordLink;
+
+    @FindBy(id = "sub_heading")
+    public WebElement resetPasswordHeader;
+
+    @FindBy (xpath = "//input[@id='email']/../label")
+    public WebElement resetPasswordMessage;
+
+    @FindBy(id = "email")
+    public WebElement emailInputBox;
+
+    @FindBy(id = "submit")
+    public WebElement resetPasswordSubmitButton;
+
+    @FindBy(id = "confirmation_message")
+    public WebElement confirmationMessage;
+
+    @FindBy(id = "success_lgn")
+    public WebElement successLogin;
+
+    @FindBy(id = "error_message")
+    public WebElement errorMessageUsername;
+
+    @FindBy(id = "error_message")
+    public WebElement errorMessagePassword;
+}
